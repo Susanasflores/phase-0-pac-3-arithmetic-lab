3fcff667006c9cc8652b5e6bc905701ce98eef5d
Susana Solis Flores <Suzy.flores@icloud.com>
2023-08-11 00:04:21 -0400

    phase-0-pac-3-arithmetic-lab

---
+++ index.js
@@ -0,0 +1,48 @@
+function add() {
+
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add(a, b) {
+    return "something";
+  }
+function add(a, b) {
+    return "something";
+  }
+  function add(a, b) {
+    return a + b;
+  } 
+  function subtract(a, b) {
+    return a - b;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  function divide(a, b) {
+    return a / b;
+  }
+  function increment(n = 1) {
+    return n += 1;
+  }
+  function decrement (n = 1) {
+    return n -= 1;
+  }
+  function makeInt(n) {
+  parseInt("2", 10);
+  }
+  function makeInt(n) {
+    return parseInt(n, 10)
+  }
+  function preserveDecimal(n = "2.222") {
+    return parseFloat(2.222)
+  }
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
